37689fec868933d85441f3c1593f5c80f682cb59
Adejuwonlo Okanlawon <adejuwonlookanlawon@gmail.com>
2023-12-12 22:58:37 -0800

    Back to see

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+* add - use to add values or characters
+* @a: Input value
+* @b: Input value
+* Return: addition of a and b
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* sub - use to subtract values or characters
+* @a: Input value
+* @b: Input value
+* Return: subtraction of a and b
+*/
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* mul - use to multiply values or characters
+* @a: Input value
+* @b: Input value
+* Return: multiplication of a and b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* divide - use to divide values or characters
+* @a: Input value
+* @b: Input value
+* Return: division of a and b
+*/
+int divide(int a, int b)
+{
+return (a / b);
+}
+
+/**
+* mod - prints out the remainder of a division
+* @a: Input value
+* @b: Input value
+* Return: remainder of a divided by b
+*/
+int mod(int a, int b)
+{
+return (a % b);
+}
